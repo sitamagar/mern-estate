9be0024a9e69e164b7e81ab101f6d696c025384d
sitamagar <msitanivrutti934@gmail.com>
2025-06-10 13:13:36 +0530

    complete sign up page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -8,9 +8,9 @@ export const signup =  async(req, res, next)=>{
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword});
     try{
-
          await newUser.save()
-         res.status(201).json("user created successfully")
+         res.status(201).json("user create successfully")
+
 
     }catch(error){
      next(error);
